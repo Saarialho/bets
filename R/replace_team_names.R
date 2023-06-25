331336af59883f1cfa502e156b44a9e1e2c188ce
Saarialho <eerosaarialho@gmail.com>
2023-06-25 21:38:08 +0300

    comi

---
+++ R/replace_team_names.R
@@ -0,0 +1,28 @@
+
+#' replaces team names in data frame
+#'
+#' @param data account name
+#' @param team1 account name
+#' @param team2 account name
+#' @param pattern account name
+#' @param replacement account name
+#' @examples
+#' hist_buch_data %>% replace_team_names(home, away, team_dictionary()$buch_name, team_dictionary()$fbref_name)
+#' @export
+replace_team_names <- function(data, team1, team2, pattern, replacement){
+
+  names <-
+    dplyr::tibble(pattern = pattern,
+           replacement = replacement) %>%
+    dplyr::filter(pattern != replacement)
+
+  # Making the named replacement vector
+  replacements <- c(names$replacement)
+  names(replacements) <- c(names$pattern)
+
+  data %>%
+    dplyr::group_by(league) %>%
+    dplyr::mutate(across(c({{team1}},{{team2}}), ~stringr::str_replace_all(., pattern = replacements))) %>%
+    dplyr::ungroup()
+}
+
